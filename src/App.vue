0d591eb2da833669dd3301b08ed6f832dec21b66
sumiship <osumi.k.osumi.k@gmail.com>
2021-10-15 00:59:15 +0900

    aaaaaa

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <div class="view-box">
       <div class="col" :style="col_style(index)" v-for="(value, index) in source" :key="index">
-        <span>{{ value }}</span>
+        <!-- <span>{{ format_number(value) }}</span> -->
         <div class="cell" :style="cell_style(value, SourceLengh - n)" v-for="n of SourceLengh" :key="n"></div>
       </div>
     </div>
     <div class="control">
-      <button @click="source_random(source)">random</button>
-      <button @click="selection_sort(source)">selectionSort</button>
-      <button @click="insertion_sort(source)">insertionSort</button>
-      <button @click="bubble_sort(source)">bubbleSort</button>
-      <input type="range" max="300" min="1" v-model="updateSpeed" />
-      swapTimeout: {{ updateSpeed }}
+      <div class="button" @click="source_random(source)">random</div>
+      <div class="button" @click="selection_sort(source)">selectionSort</div>
+      <div class="button" @click="insertion_sort(source)">insertionSort</div>
+      <div class="button" @click="bubble_sort(source)">bubbleSort</div>
+      <div class="button" @click="quick_sort(source, 0, SourceLengh - 1)">quickSort</div>
+    </div>
+    <div class="range">
+      speed:{{ 501 - updateSpeed }}<br />
+      <input type="range" max="500" min="1" v-model="updateSpeed" />
     </div>
   </div>
 </template>
@@ -25,8 +28,13 @@ export default class App extends Vue {
   source: number[] = [];
   searching: number[] = [];
   checking: number[] = [];
-  SourceLengh = 40;
+  SourceLengh = 80;
   updateSpeed = 70;
+
+  format_number(num: number): string {
+    if (num < 10) return `0${num}`;
+    return String(num);
+  }
 
   col_style(index: number): string {
     return this.searching.indexOf(index) != -1 ? "background-color: orange" : this.checking.indexOf(index) != -1 ? "background-color: red" : "";
@@ -102,6 +110,48 @@ export default class App extends Vue {
     this.searching = [-1];
     this.checking = [-1];
   }
+
+  async quick_sort(arr: number[], begin: number, end: number): Promise<void> {
+    if (begin >= end) return;
+    const pivot = arr[begin];
+
+    this.checking = [];
+    for (let t = begin; t <= end; t++) {
+      this.checking.push(t);
+    }
+    let check = 0;
+    let i = begin;
+    let j = end;
+    this.searching = [i, j];
+    // console.log("------------");
+    // console.log(begin, end);
+    // console.log(JSON.parse(JSON.stringify(arr)));
+
+    while (i <= j) {
+      while (i <= end && arr[i] < pivot) {
+        i++;
+        await this.update_searching([i, j]);
+      }
+      while (i >= begin && arr[j] >= pivot) {
+        j--;
+        await this.update_searching([i, j]);
+      }
+      // console.log(`check${i},${j}`);
+      if (i > j) break;
+      // console.log(`swap${i},${j}`);
+      check++;
+      this.swap(i++, j--, arr);
+      await this.update_source(arr);
+    }
+    // console.log(i, j);
+    // console.log(JSON.parse(JSON.stringify(arr)));
+    await this.quick_sort(arr, begin, i - 1);
+    if (check == 0) i++;
+    await this.quick_sort(arr, i, end);
+    this.searching = [-1];
+    this.checking = [-1];
+  }
+
   async source_random(arr: number[]): Promise<void> {
     for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -118,7 +168,7 @@ export default class App extends Vue {
     return source;
   }
 
-  mounted() {
+  mounted(): void {
     this.source = this.make_source(this.SourceLengh);
     // console.log(this.source);
   }
@@ -130,7 +180,7 @@ export default class App extends Vue {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
-  font-size: 2rem;
+  font-size: 1rem;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -152,5 +202,23 @@ export default class App extends Vue {
 .cell {
   padding-top: 100%;
   border: 1px solid black;
+}
+.control {
+  display: flex;
+}
+.button {
+  text-align: center;
+  font-size: 2rem;
+  border: 1px solid red;
+  width: 100%;
+}
+.button:hover {
+  background-color: pink;
+}
+.range {
+  font-size: 4rem;
+}
+.range input {
+  width: 50%;
 }
 </style>
